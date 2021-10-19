4bcce9c9f88953a9108889dcee69356a4116b7ca
Eik <etty.ikpeme@gmail.com>
2021-10-19 15:06:13 -0500

    Add styles to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Head from "next/head";
-
+import useStyles from "../utils/styles"
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
 
 const Layout = ({ children }) => {
+    const classes = useStyles()
   return (
     <div>
       <Head>
         <title> Next Ecommerce</title>
       </Head>
-      <AppBar position="static">
+      <AppBar className={classes.navbar}position="static">
         <Toolbar>
           <Typography> Ecommerce </Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
           <Typography>
               All rights reserved, Next Amazona.
           </Typography>
