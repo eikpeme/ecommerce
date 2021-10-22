3ff56f0c9773755ba0059c990799225f900368cf
Eik <etty.ikpeme@gmail.com>
2021-10-22 17:46:17 -0500

    cookies and state.cart cleared, oder model and pot request with order

--- pages/placeOrder.js
+++ pages/placeOrder.js
@@ -1,80 +1,97 @@
 import React, { useContext, useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 import Layout from "../components/Layout";
 import { Store } from "../utils/Store";
+import NextLink from "next/link";
 import Image from "next/image";
-import dynamic from "next/dynamic";
-import axios from "axios";
 import {
   Grid,
   TableContainer,
   Table,
-  TableCell,
-  TableBody,
-  TableHead,
-  TableRow,
   Typography,
-  Select,
-  MenuItem,
-  Button,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
   Link,
-  ListItem,
-  List,
+  CircularProgress,
+  Button,
   Card,
+  List,
+  ListItem,
 } from "@material-ui/core";
-import NextLink from "next/link";
-import { Router, useRouter } from "next/router";
+import axios from "axios";
+import { useRouter } from "next/router";
 import useStyles from "../utils/styles";
 import CheckoutWizard from "../components/CheckoutWizard";
 import { useSnackbar } from "notistack";
+import { getError } from "../utils/error";
+import Cookies from "js-cookie";
 
 function PlaceOrder() {
+  const classes = useStyles();
   const router = useRouter();
   const { state, dispatch } = useContext(Store);
-  const classes = useStyles();
-  const { closeSnackbar, enqueueSnackbar } = useSnackbar();
-  const [loading, setLoading] = useState(false);
   const {
-    cart: { cartItems },
-    cart: { shippingAddress },
-    cart: { paymentMethod },
+    userInfo,
+    cart: { cartItems, shippingAddress, paymentMethod },
   } = state;
-
-  const { fullName, address, city, postalCode, country } = shippingAddress;
-
-  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100; //123.456 => 123.46
-  const itemsPrice = cartItems.reduce(
-    (total, value) => total + value.price * value.quantity,
-    0
+  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100; // 123.456 => 123.46
+  const itemsPrice = round2(
+    cartItems.reduce((a, c) => a + c.price * c.quantity, 0)
   );
-
-  const ShippingPrice = itemsPrice > 200 ? 0 : 15;
-
+  const shippingPrice = itemsPrice > 200 ? 0 : 15;
   const taxPrice = round2(itemsPrice * 0.15);
-  const totalPrice = round2(itemsPrice + ShippingPrice + taxPrice);
+  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
 
   useEffect(() => {
     if (!paymentMethod) {
-      router.push("//payment");
+      router.push("/payment");
+    }
+    if (cartItems.length === 0) {
+      router.push("/cart");
     }
   }, []);
-
-  const placeOrderHandler = () => {
+  const { closeSnackbar, enqueueSnackbar } = useSnackbar();
+  const [loading, setLoading] = useState(false);
+  const placeOrderHandler = async () => {
     closeSnackbar();
     try {
       setLoading(true);
-    } catch (error) {
+      const { data } = await axios.post(
+        "/api/orders",
+        {
+          orderItems: cartItems,
+          shippingAddress,
+          paymentMethod,
+          itemsPrice,
+          shippingPrice,
+          taxPrice,
+          totalPrice,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${userInfo.token}`,
+          },
+        }
+      );
+      dispatch({ type: "CART_CLEAR" });
+      Cookies.remove("cartItems");
       setLoading(false);
-      enqueueSnackbar(getError(error), { variant: "error" });
+      router.push(`/`);
+      //   router.push(`/order/${data._id}`);
+    } catch (err) {
+      setLoading(false);
+      enqueueSnackbar(getError(err), { variant: "error" });
     }
-    router.push("/confirmationPage");
   };
-
   return (
     <Layout title="Place Order">
       <CheckoutWizard activeStep={3}></CheckoutWizard>
       <Typography component="h1" variant="h1">
         Place Order
       </Typography>
+
       <Grid container spacing={1}>
         <Grid item md={9} xs={12}>
           <Card className={classes.section}>
@@ -85,7 +102,9 @@ function PlaceOrder() {
                 </Typography>
               </ListItem>
               <ListItem>
-                {fullName}, {address}, {city}, {postalCode}, {country}
+                {shippingAddress.fullName}, {shippingAddress.address},{" "}
+                {shippingAddress.city}, {shippingAddress.postalCode},{" "}
+                {shippingAddress.country}
               </ListItem>
             </List>
           </Card>
@@ -121,19 +140,20 @@ function PlaceOrder() {
                       {cartItems.map((item) => (
                         <TableRow key={item._id}>
                           <TableCell>
-                            <NextLink passHref href={`/product/${item.slug}`}>
+                            <NextLink href={`/product/${item.slug}`} passHref>
                               <Link>
                                 <Image
                                   src={item.image}
                                   alt={item.name}
                                   width={50}
                                   height={50}
-                                />
+                                ></Image>
                               </Link>
                             </NextLink>
                           </TableCell>
+
                           <TableCell>
-                            <NextLink passHref href={`/product/${item.slug}`}>
+                            <NextLink href={`/product/${item.slug}`} passHref>
                               <Link>
                                 <Typography>{item.name}</Typography>
                               </Link>
@@ -163,30 +183,30 @@ function PlaceOrder() {
               <ListItem>
                 <Grid container>
                   <Grid item xs={6}>
-                    <Typography> Items:</Typography>
+                    <Typography>Items:</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography align="right">{itemsPrice}</Typography>
-                  </Grid>
-                </Grid>
-              </ListItem>
-              <ListItem>
-                <Grid container>
-                  <Grid item xs={6}>
-                    <Typography> Tax:</Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography align="right">{taxPrice}</Typography>
+                    <Typography align="right">${itemsPrice}</Typography>
                   </Grid>
                 </Grid>
               </ListItem>
               <ListItem>
                 <Grid container>
                   <Grid item xs={6}>
-                    <Typography> shipping:</Typography>
+                    <Typography>Tax:</Typography>
                   </Grid>
                   <Grid item xs={6}>
-                    <Typography align="right">{ShippingPrice}</Typography>
+                    <Typography align="right">${taxPrice}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Grid container>
+                  <Grid item xs={6}>
+                    <Typography>Shipping:</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography align="right">${shippingPrice}</Typography>
                   </Grid>
                 </Grid>
               </ListItem>
@@ -194,7 +214,7 @@ function PlaceOrder() {
                 <Grid container>
                   <Grid item xs={6}>
                     <Typography>
-                      <strong>Total: </strong>
+                      <strong>Total:</strong>
                     </Typography>
                   </Grid>
                   <Grid item xs={6}>
@@ -206,32 +226,23 @@ function PlaceOrder() {
               </ListItem>
               <ListItem>
                 <Button
+                  onClick={placeOrderHandler}
                   variant="contained"
                   color="primary"
                   fullWidth
-                  onClick={placeOrderHandler}
                 >
                   Place Order
                 </Button>
               </ListItem>
+              {loading && (
+                <ListItem>
+                  <CircularProgress />
+                </ListItem>
+              )}
             </List>
           </Card>
         </Grid>
       </Grid>
-      <List>
-        <ListItem>
-          <Button
-            variant="contained"
-            fullWidth
-            color="secondary"
-            onClick={() => {
-              router.push("/payment");
-            }}
-          >
-            Back
-          </Button>
-        </ListItem>
-      </List>
     </Layout>
   );
 }
